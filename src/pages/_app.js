c49832064f2f9ffb898d2b67c7b08d37f1f62937
chonger878 <echong06@gmail.com>
2022-12-14 12:49:15 -0800

    latest changes to app.js

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -1,6 +1,7 @@
 import '../styles/globals.css'
 import { GlobalStyles } from '../global';
 import { theme } from '../theme';
+import { Burger, Menu } from '../components';
 
 function MyApp({ Component, pageProps }) {
   return <Component {...pageProps} />
