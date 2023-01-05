dca365a9102d9476d9d16ac4d8b9fa5337e513ad
chonger878 <echong06@gmail.com>
2023-01-05 02:04:14 -0800

    Modified burger.js file

--- src/components/Burger/Burger.js
+++ src/components/Burger/Burger.js
@@ -1,14 +1,17 @@
-import React from 'react';
-import { StyledBurger } from './Burger.styled';
+import Link from 'next/link'
+import { slide as Menu } from 'react-burger-menu'
+const HamburgerMenu = () => (<div className='relative p-2'>
+    <Menu customBurgerIcon={<HamburgerIcon />} width={'auto'} className='left-0 top-12' >
+        <Links />
+    </Menu>
+</div>)
 
-const Burger = ({ open,setOpen }) => {
-  return (
-    <StyledBurger open={open} onClick={() => setOpen(!open)}>
-      <div />
-      <div />
-      <div />
-    </StyledBurger>
-  )
-}
+const HamburgerIcon = () => (<div className='p-1/2'><svg className="w-8 h-8 text-gray-500" fill="none" strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" viewBox="0 0 24 24" stroke="currentColor"><path d="M4 6h16M4 12h16M4 18h16"></path></svg></div>)
 
-export default Burger;
+export const Links = () => (<>
+    <Link href="/"><a className='font-bold p-4'>Home</a></Link>
+    <Link href="/about"><a className='font-bold p-4'>About</a></Link>
+    <Link href="/about"><a className='font-bold p-4'>Discord</a></Link>
+</>)
+
+export default HamburgerMenu
