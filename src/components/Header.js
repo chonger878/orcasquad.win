ea1cc120398a7e2c2668855851fdf4a17b4be175
chonger878 <echong06@gmail.com>
2023-01-06 01:24:33 -0800

    Deleted some unneeded files and changed Header.JS

--- src/components/Header.js
+++ src/components/Header.js
@@ -20,6 +20,12 @@ export default function Header() {
                                 <a href="#">Discord</a>
                             </li>
                         </ul>
+                        <div className='max-w-full h-12 flex justify-start items-center bg-black mb-4 text-white rounded-md '>
+                            <div className='flex md:hidden'><HamburgerMenu /></div>
+                            <div className='hidden md:flex'>
+                                <Links />
+                            </div>
+                        </div>
                     </nav>
                 </div>
             </header>
--- src/pages/index.js
+++ src/pages/index.js
@@ -4,7 +4,7 @@ import Header from '../components/Header'
 import Image from 'next/image'
 import styles from '../styles/Home.module.css'
 import HamburgerMenu, {Links} from '../components/Burger/Burger';
-import Menu from '../components/Menu/Menu.js';
+import '../styles/burger.css'
 
 export default function Home() {
     const [open,setOpen] = useState(false);
@@ -35,12 +35,6 @@ export default function Home() {
 
         </Head>
         <Header>
-            <div className='max-w-full h-12 flex justify-start items-center bg-black mb-4 text-white rounded-md '>
-                <div className='flex md:hidden'><HamburgerMenu /></div>
-                <div className='hidden md:flex'>
-                    <Links />
-                </div>
-            </div>
             
         </Header>
 
